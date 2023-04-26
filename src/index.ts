4c436dd97d573c4e4ca4acaba9b72b81c44df1b4
Vivien Le Duc <leducvivien@gmail.com>
2023-04-26 18:27:31 +0200

    chore: deleted src files and test files

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { PackageJson, Bug, Directories, Funding, PeerDependencyMeta, Person, Repository } from "./PackageJson";
-import {Pakaje} from "./Pakaje";
-export { PackageJson, Bug, Directories, Funding, PeerDependencyMeta, Person, Repository, Pakaje };
